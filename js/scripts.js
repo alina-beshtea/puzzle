4e8612cb5b9f838d1c775b87ba09d9f2a3e98907
Student <example@example.com>
2019-10-29 15:14:09 -0700

    add js scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,31 @@
 $(document).ready(function() {
   $("form#solve").submit(function() {
     event.preventDefault();
-    
+
+    var sentence = $("input#sentence").val();
+    var letters = sentence.split("");
+
+    var vowels = ["a", "A", "e", "E", "i", "I", "o", "O", "u", "U"];
+
+    for (i=0; i<letters.length; i += 1) {
+      if (vowels.includes(letters[i])) {
+        letters[i] = "-";
+
+        $("#output").text(letters.join(""));
+      }
+    };
+
+    // var array = letters.map(function(letter) {
+    //   if (vowels.includes(letter)) {
+    //     return letter = "-";
+    //   } else {
+    //     return letter;
+    //   }
+    // });
+      // console.log(array.join(""));
+
+      // console.log(letters);
+      // console.log(array);
+
   });
 });
